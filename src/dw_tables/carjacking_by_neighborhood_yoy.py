9baee455b3dab28f01431ab21ce8b6b280caee13
hackerlikecomputer <hackerlikecomputer@gmail.com>
2021-08-16 11:15:44 -0500

    use absolute values for decreases

--- src/dw_tables/carjacking_by_neighborhood_yoy.py
+++ src/dw_tables/carjacking_by_neighborhood_yoy.py
@@ -13,11 +13,11 @@ def tooltip_comparison_text(row):
     if row[2021] == row[2020]:
         return ("the same number as", "black")
     else:
-        dif = abs(int(row[2021] - row[2020]))
+        dif = int(row[2021] - row[2020])
         if dif > 0:
             return (f"{dif} more than", "#cc281d")
         else:
-            return (f"{dif} fewer than", "#009c1d")
+            return (f"{abs(dif)} fewer than", "#009c1d")
 
 xt["tooltip_comparison_text"] = xt.apply(tooltip_comparison_text, axis=1)
 xt["tooltip_comparison_text_color"] = xt.tooltip_comparison_text.apply(lambda t: t[1])
