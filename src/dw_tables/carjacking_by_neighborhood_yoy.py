a8b8a4d95fc227b8b95981979d55c387a2dee040
hackerlikecomputer <hackerlikecomputer@gmail.com>
2021-08-16 11:12:52 -0500

    fill nan values before creating tooltip info

--- src/dw_tables/carjacking_by_neighborhood_yoy.py
+++ src/dw_tables/carjacking_by_neighborhood_yoy.py
@@ -6,19 +6,18 @@ df = read_parse_date(sys.argv)
 df["year"] = df.date.dt.year
 xt = pd.crosstab(
     index=df.pri_neigh, columns=df.year, values=df.case_number, aggfunc="nunique"
-)
+).fillna(0)
 
 
 def tooltip_comparison_text(row):
     if row[2021] == row[2020]:
         return ("the same number as", "black")
     else:
-        dif = row[2021] - row[2020]
+        dif = abs(int(row[2021] - row[2020]))
         if dif > 0:
             return (f"{dif} more than", "#cc281d")
         else:
             return (f"{dif} fewer than", "#009c1d")
-
 
 xt["tooltip_comparison_text"] = xt.apply(tooltip_comparison_text, axis=1)
 xt["tooltip_comparison_text_color"] = xt.tooltip_comparison_text.apply(lambda t: t[1])
