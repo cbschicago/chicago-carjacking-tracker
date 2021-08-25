fbf75f6ac7bc42866e3afdba54f0055e999f27d3
Chris Hacker <49451910+hackerlikecomputer@users.noreply.github.com>
2021-08-25 11:18:04 -0500

    Fix column name mismatch

--- src/merge_ca_name.py
+++ src/merge_ca_name.py
@@ -5,7 +5,7 @@ from util import print_df
 
 df = pd.read_csv(sys.argv[1]).rename(columns={"lon": "Lon", "lat": "Lat"})
 df = gpd.GeoDataFrame(
-    df, geometry=gpd.points_from_xy(df.lon, df.lat), crs="EPSG:4326"
+    df, geometry=gpd.points_from_xy(df.Lon, df.Lat), crs="EPSG:4326"
 )
 geo = gpd.read_file(sys.argv[2])
 df = df.to_crs("EPSG:4326")
