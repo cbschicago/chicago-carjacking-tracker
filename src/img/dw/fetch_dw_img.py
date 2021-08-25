8ab7e081e442ea1a9df3ec3de9dc6c556488571f
hackerlikecomputer <hackerlikecomputer@gmail.com>
2021-08-25 16:08:05 -0500

    bump image scale

--- src/img/dw/fetch_dw_img.py
+++ src/img/dw/fetch_dw_img.py
@@ -9,4 +9,4 @@ chart_id = sys.argv[2]
 filepath = sys.argv[3]
 # add data to force chart refresh
 dw.add_data(chart_id, df)
-dw.export_chart(chart_id=chart_id, filepath=filepath, unit="px", width=800)
+dw.export_chart(chart_id=chart_id, filepath=filepath, unit="px", width=800, scale=2)
