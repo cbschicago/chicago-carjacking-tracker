1e93b8d83e77966d7c5db06c7339abca5dbd07b1
hackerlikecomputer <hackerlikecomputer@gmail.com>
2021-09-29 10:49:14 -0500

    add openpyxl dependency

--- setup.py
+++ setup.py
@@ -5,12 +5,14 @@ setup(
     package_dir={"": "src"},
     install_requires=[
         "csvkit==1.0.6",
-        "pandas==1.3.1",
-        "geopandas==0.9.0",
-        "Rtree==0.9.7",
         "datawrapper==0.4.6",
+        "geopandas==0.9.0",
         "matplotlib==3.4.3",
         "matplotlib-inline==0.1.2",
+        "openpyxl==3.0.7",
+        "pandas==1.3.1",
+        "Rtree==0.9.7",
+        "tabulate==0.8.9",
         "XlsxWriter==3.0.1",
     ],
 )
